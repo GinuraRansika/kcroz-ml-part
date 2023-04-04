0c492c2a6a50adac4eda59532e48958bdfe72b3b
Ginura Ransika De Seram <ginuraransika@gmail.com>
2023-04-04 17:36:29 +0530

    Edited

--- main.py
+++ main.py
@@ -1,39 +1,47 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI,Request
 # from pydantic import BaseModel
 import uvicorn
-# import numpy as np
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
+import numpy as np
+from transformers import BertTokenizer, BertForSequenceClassification
 import torch
 
-
 app = FastAPI()
-gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
 
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/hello")
+def read_root():
+    return {"Hello": "Hello"}
 
 def get_model():
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model = "Ginura/kcroz-summerization-model"
-    tokenizer = AutoTokenizer.from_pretrained(model)
-    kcrozModel= AutoModelForSeq2SeqLM.from_pretrained(model).to(device)
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = BertForSequenceClassification.from_pretrained("pnichite/YTFineTuneBert")
+    return tokenizer,model
 
-    pipe = pipeline("summarization", model=model)
-    return tokenizer,kcrozModel, pipe
+d = {
+    
+  1:'Toxic',
+  0:'Non Toxic'
+}
 
-tokenizer, model, pipe = get_model()
+tokenizer,model = get_model()
 
-
-@app.post("/summerize")
+@app.post("/predict")
 async def read_root(request: Request):
     data = await request.json()
     print(data)
-    if 'text'in data:
+    if 'text' in data:
         user_input = data['text']
-        output= pipe(user_input, **gen_kwargs)[0]['summary_text']
-        response = {"recieved text": user_input, "summary text": output}
+        test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=512,return_tensors='pt')
+        output = model(**test_sample)
+        y_pred = np.argmax(output.logits.detach().numpy(),axis=1)  
+        response = {"Recieved Text": user_input,"Prediction": d[y_pred[0]]}
     else:
-        response = {"Nothing"}
+        response = {"Recieved Text": "No Text Found"}
     return response
 
-
 if __name__ == "__main__":
-    uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run("main:app",host='0.0.0.0', port=8080, reload=True, debug=True)
