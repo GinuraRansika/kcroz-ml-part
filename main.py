733f47c147abd4fd913775a463bfec396d3b0069
Ginura Ransika De Seram <ginuraransika@gmail.com>
2023-04-06 08:52:55 +0530

    Changed the post method to get method

--- main.py
+++ main.py
@@ -9,41 +9,42 @@ import newspaper
 
 
 app = FastAPI()
-gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
+gen_kwargs = {"length_penalty": 0.8, "num_beams": 8, "max_length": 128}
 
 
 def get_model():
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = "Ginura/kcroz-summerization-model"
     tokenizer = AutoTokenizer.from_pretrained(model)
-    kcrozModel= AutoModelForSeq2SeqLM.from_pretrained(model).to(device)
+    kcrozModel = AutoModelForSeq2SeqLM.from_pretrained(model).to(device)
+
     pipe = pipeline("summarization", model=model)
-    return tokenizer,kcrozModel, pipe
+    return tokenizer, kcrozModel, pipe
+
 
 tokenizer, model, pipe = get_model()
 
 
-@app.post("/summerize")
-async def read_root(request: Request):
-    data = await request.json()
-    if 'text'in data:
-        user_interest = data['text']
-        newspaper_url = get_article_url(user_interest)
-        article_content = get_newspaper_content(newspaper_url)
-        output= pipe(article_content, **gen_kwargs)[0]['summary_text']
-        response = output
-    else:
-        response = {"Sorry!, Somehing went wrong"}
+@app.get("/summerize/{user_interest}")
+async def read_root(user_interest: str):
+    print(user_interest)
+    newspaper_url = get_article_url(user_interest)
+    print(newspaper_url)
+    article_content = get_newspaper_content(newspaper_url)
+    output = pipe(article_content, **gen_kwargs)[0]['summary_text']
+    response = output
     return response
+
 
 def get_article_url(user_interest):
     newsapi = NewsApiClient(api_key='ad2c008f0e354dd38de6c27d44d057eb')
     sources = newsapi.get_sources()
-    top_headlines = newsapi.get_everything(q=user_interest,language='en')
+    top_headlines = newsapi.get_everything(q=user_interest, language='en')
     articles = top_headlines['articles']
     articles_dict = articles[0]
     url = (articles_dict['url'])
     return url
+
 
 def get_newspaper_content(url):
     article_object = newspaper.Article(url)
@@ -51,6 +52,7 @@ def get_newspaper_content(url):
     article_object.parse()
     article_content = article_object.text
     return article_content
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host='0.0.0.0', port=8080, reload=True)
